c035a4b3e500345e1bb06cbc8cc3d1024a5a4fc7
elvin <elvinyeka@gmail.com>
2022-08-15 10:30:46 +0400

    test

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ *  SASS Sintactically Awesome Style Sheets
+ * 2006 ci ilde Hampton Catlin yaradilmishdir. ruby dilinin sintaksisine benzeyir.
+ *
+ * ! 1. deyishenler - variables
+ *
+ *
+ * ! 2. nesting
+ * & - ampersand
+ *
+ * mixin and icludes
+ *
+ *! 7-1 pattern
+ *
+ *
+ */
